1c9a6858a628b92a9263875bae2fd6a9e9a8eb5d
twoyoung <yyoung1025@gmail.com>
2023-07-28 20:27:50 +1200

    test

--- car-rental-webapp/app.py
+++ car-rental-webapp/app.py
@@ -1,27 +1,6 @@
 from flask import Flask
-from flask import render_template
-from flask import request
-from flask import redirect
-from flask import url_for
-import re
-from datetime import datetime
-import mysql.connector
-from mysql.connector import FieldType
-import connect
 
 app = Flask(__name__)
-
-dbconn = None
-connection = None
-
-def getCursor():
-    global dbconn
-    global connection
-    connection = mysql.connector.connect(user=connect.dbuser, \
-    password=connect.dbpass, host=connect.dbhost, \
-    database=connect.dbname, autocommit=True)
-    dbconn = connection.cursor()
-    return dbconn
 
 # default / base page
 @app.route("/")
