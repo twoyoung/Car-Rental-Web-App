7e0291e7e354d4819c7e7bbad6601669693c6527
twoyoung <yyoung1025@gmail.com>
2023-07-29 14:59:31 +1200

    Update rental.sql

--- rental.sql
+++ rental.sql
@@ -7,6 +7,7 @@ CREATE TABLE IF NOT EXISTS users
 UserName VARCHAR(50) NOT NULL,
 Password VARCHAR(20) NOT NULL,
 Email VARCHAR(80) NOT NULL,
+Role INT,
 PRIMARY KEY (UserName)
 );
 
@@ -18,7 +19,6 @@ ProfileName VARCHAR(80),
 Address VARCHAR(80),
 Email VARCHAR(80) NOT NULL,
 PhoneNumber VARCHAR(20),
-Role INT,
 PRIMARY KEY (UserID),
 FOREIGN KEY (UserName) REFERENCES users(UserName)
 ON UPDATE CASCADE
@@ -40,25 +40,25 @@ FOREIGN KEY (UserID) REFERENCES accountprofile(UserID)
 
 
 INSERT INTO users VALUES 
-("customer1","customer1","customer1@gamil.com"),
-("customer2","customer2","customer2@gmail.com"),
-("customer3","customer3","customer3@gmail.com"),
-("customer4","customer4","customer4@gmail.com"),
-("customer5","customer5","customer5@gmail.com"),
-("admin","admin","admin@gmail.com"),
-("staff1","staff1","staff1@gmail.com"),
-("staff2","staff2","staff2@gmail.com"),
-("staff3","staff3","staff3@gmail.com");
+("customer1","customer1","customer1@gamil.com",NULL),
+("customer2","customer2","customer2@gmail.com",NULL),
+("customer3","customer3","customer3@gmail.com",NULL),
+("customer4","customer4","customer4@gmail.com",NULL),
+("customer5","customer5","customer5@gmail.com",NULL),
+("admin","admin","admin@gmail.com",1),
+("staff1","staff1","staff1@gmail.com",2),
+("staff2","staff2","staff2@gmail.com",2),
+("staff3","staff3","staff3@gmail.com",2);
 
 INSERT INTO accountprofile VALUES 
-(000001,"customer1","customername1","auckland 1000","customer1@gmail.com","02100001",NULL),
-(00002,"customer2","customername2","auckland bilingol street","customer2@gmail.com","02200002",NULL),
-(00003,"customer3","customername3","auckland woolsworth street","customer3@gmail.com","02800003",NULL),
-(00004,"customer4","customername4","auckland kate street", "customer4@gmail.com","02100004",NULL),
-(00005,"customer5","customername5","auckland lake street", "customer5@gmail.com","02100005",NULL),
-(006,"staff1","staffname1","auckland bike street","staff1@gmail.com","02100221",1),
-(007,"staff2","staffname2","auckland desk street","staff2@gmail.com","02100222",1),
-(008,"staff3","staffname3","auckland window street","staff3@gmail.com","02100223",1);
+(000001,"customer1","customername1","auckland 1000","customer1@gmail.com","02100001"),
+(00002,"customer2","customername2","auckland bilingol street","customer2@gmail.com","02200002"),
+(00003,"customer3","customername3","auckland woolsworth street","customer3@gmail.com","02800003"),
+(00004,"customer4","customername4","auckland kate street", "customer4@gmail.com","02100004"),
+(00005,"customer5","customername5","auckland lake street", "customer5@gmail.com","02100005"),
+(006,"staff1","staffname1","auckland bike street","staff1@gmail.com","02100221"),
+(007,"staff2","staffname2","auckland desk street","staff2@gmail.com","02100222"),
+(008,"staff3","staffname3","auckland window street","staff3@gmail.com","02100223");
 
 INSERT INTO car VALUES
 (00001,"Toyota Estima",2003,"WSJ788",8,300.5,NULL),
