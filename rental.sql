783a7ed146b8a0f407ed0efb8601e2167156e66a
twoyoung <yyoung1025@gmail.com>
2023-07-29 15:07:25 +1200

    database update

--- rental.sql
+++ rental.sql
@@ -2,27 +2,17 @@ DROP SCHEMA IF EXISTS rental;
 CREATE schema rental;
 USE rental;
 
-CREATE TABLE IF NOT EXISTS users
-(
-UserName VARCHAR(50) NOT NULL,
-Password VARCHAR(20) NOT NULL,
-Email VARCHAR(80) NOT NULL,
-Role INT,
-PRIMARY KEY (UserName)
-);
-
-CREATE TABLE IF NOT EXISTS accountprofile
+CREATE TABLE IF NOT EXISTS user
 (
 UserID INT auto_increment NOT NULL,
 UserName VARCHAR(50) NOT NULL,
+Password VARCHAR(20) NOT NULL,
+Email VARCHAR(80) NOT NULL,
 ProfileName VARCHAR(80),
 Address VARCHAR(80),
-Email VARCHAR(80) NOT NULL,
 PhoneNumber VARCHAR(20),
-PRIMARY KEY (UserID),
-FOREIGN KEY (UserName) REFERENCES users(UserName)
-ON UPDATE CASCADE
-ON DELETE CASCADE
+Role INT,
+PRIMARY KEY (UserID)
 );
 
 CREATE TABLE IF NOT EXISTS car
@@ -35,30 +25,19 @@ SeatCap INT NOT NULL,
 RentalPerDay float NOT NULL,
 UserID INT,
 PRIMARY KEY (CarID),
-FOREIGN KEY (UserID) REFERENCES accountprofile(UserID)
+FOREIGN KEY (UserID) REFERENCES user(UserID)
 );
 
-
-INSERT INTO users VALUES 
-("customer1","customer1","customer1@gamil.com",NULL),
-("customer2","customer2","customer2@gmail.com",NULL),
-("customer3","customer3","customer3@gmail.com",NULL),
-("customer4","customer4","customer4@gmail.com",NULL),
-("customer5","customer5","customer5@gmail.com",NULL),
-("admin","admin","admin@gmail.com",1),
-("staff1","staff1","staff1@gmail.com",2),
-("staff2","staff2","staff2@gmail.com",2),
-("staff3","staff3","staff3@gmail.com",2);
-
-INSERT INTO accountprofile VALUES 
-(000001,"customer1","customername1","auckland 1000","customer1@gmail.com","02100001"),
-(00002,"customer2","customername2","auckland bilingol street","customer2@gmail.com","02200002"),
-(00003,"customer3","customername3","auckland woolsworth street","customer3@gmail.com","02800003"),
-(00004,"customer4","customername4","auckland kate street", "customer4@gmail.com","02100004"),
-(00005,"customer5","customername5","auckland lake street", "customer5@gmail.com","02100005"),
-(006,"staff1","staffname1","auckland bike street","staff1@gmail.com","02100221"),
-(007,"staff2","staffname2","auckland desk street","staff2@gmail.com","02100222"),
-(008,"staff3","staffname3","auckland window street","staff3@gmail.com","02100223");
+INSERT INTO user VALUES 
+(1,"admin","admin","admin@gmail.com",NULL,NULL,NULL,1)
+(2,"customer1","customer1","customer1@gamil.com","customername1","auckland 1000","02100001",NULL),
+(3,"customer2","customer2","customer2@gmail.com","customername2","auckland bilingol street","02200002",NULL),
+(4,"customer3","customer3","customer3@gmail.com","customername3","auckland woolsworth street","02800003",NULL),
+(5,"customer4","customer4","customer4@gmail.com","customername4","auckland kate street","02100004",NULL),
+(6,"customer5","customer5","customer5@gmail.com","customername5","auckland lake street","02100005",NULL),
+(7,"staff1","staffname1","auckland bike street","staff1@gmail.com","02100221",2),
+(8,"staff2","staffname2","auckland desk street","staff2@gmail.com","02100222",2),
+(9,"staff3","staffname3","auckland window street","staff3@gmail.com","02100223",2);
 
 INSERT INTO car VALUES
 (00001,"Toyota Estima",2003,"WSJ788",8,300.5,NULL),
