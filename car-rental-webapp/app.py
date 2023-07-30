be2df7f47f079eb27a23ad3b931b0a5da57f948b
twoyoung <yyoung1025@gmail.com>
2023-07-30 20:14:10 +1200

    fix bug

--- car-rental-webapp/app.py
+++ car-rental-webapp/app.py
@@ -23,17 +23,15 @@ mysql = MySQL(app)
 def is_authenticated():
     return 'loggedin' in session
 
-def get_user_role():
-    # In a real application, you would likely retrieve the user's role from a database.
-    # For this example, let's assume a user with the username 'admin' has the role 'admin'.
-    if 'loggedin' in session:
-        if session['role'] == 1:
-            return 'admin'
-        elif session['role'] is NULL :
-            return 'customer'
-        elif session['role'] == 2:
-            return 'staff'
-    return None
+# def get_user_role():
+#     if session['role'] == 1:
+#         return 'admin'
+#     elif session['role'] is NULL :
+#         return 'customer'
+#     elif session['role'] == 2:
+#         return 'staff'
+#     else:
+#         return None
 
 # http://localhost:5000/login/ - this will be the login page, we need to use both GET and POST requests
 @app.route('/login/', methods=['GET', 'POST'])
@@ -127,17 +125,25 @@ def home():
         # User is loggedin show them the home page
 
         # Get the user's role.
-        user_role = get_user_role()
-
-        # Check if the user's role is allowed to access this page.
-        if user_role == 'admin':
+        # user_role = get_user_role()
+        if session['role'] == 1:
             return render_template('admin_home.html', username=session['username'])
-        elif user_role == 'staff':
-            return render_template('staff_home.html', username=session['username'])
-        elif user_role == 'customer':
+        elif session['role'] is NULL :
             return render_template('customer_home.html', username=session['username'])
+        elif session['role'] == 2:
+            return render_template('staff_home.html', username=session['username'])
         else:
             return "Unauthorized"
+
+        # Check if the user's role is allowed to access this page.
+        # if user_role == 'admin':
+            
+        # elif user_role == 'staff':
+            
+        # elif user_role == 'customer':
+            
+        # else:
+            
     else:
     # User is not loggedin redirect to login page
         return redirect(url_for('login'))
