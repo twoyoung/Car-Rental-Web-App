00f005fbc7dfee8fc6dccef21034c6b5e6964788
twoyoung <yyoung1025@gmail.com>
2023-08-03 20:45:21 +1200

    Update app.py

--- car-rental-webapp/app.py
+++ car-rental-webapp/app.py
@@ -10,18 +10,18 @@ app = Flask(__name__)
 # Change this to your secret key (can be anything, it's for extra protection)
 app.secret_key = 'Young32494971'
 
-# Enter your database connection details below
-# app.config['MYSQL_HOST'] = '2young.mysql.pythonanywhere-services.com'
-# app.config['MYSQL_USER'] = '2young'
-# app.config['MYSQL_PASSWORD'] = 'Young@32494971'
-# app.config['MYSQL_DB'] = '2young$COMP639'
-# app.config['MYSQL_PORT'] = 3306
-
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'rental'
+#Enter your database connection details below
+app.config['MYSQL_HOST'] = '2young.mysql.pythonanywhere-services.com'
+app.config['MYSQL_USER'] = '2young'
+app.config['MYSQL_PASSWORD'] = 'Young@32494971'
+app.config['MYSQL_DB'] = '2young$COMP639'
 app.config['MYSQL_PORT'] = 3306
+
+# app.config['MYSQL_HOST'] = 'localhost'
+# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = ''
+# app.config['MYSQL_DB'] = 'rental'
+# app.config['MYSQL_PORT'] = 3306
 
 # Intialize MySQL
 mysql = MySQL(app)
