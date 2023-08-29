2485c7359e8a55bed7c4f7e11aae616172092222
twoyoung <yyoung1025@gmail.com>
2023-08-30 07:52:15 +1200

    Update __init__.py

--- car_rental_webapp/__init__.py
+++ car_rental_webapp/__init__.py
@@ -2,8 +2,22 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# Change this to your secret key (can be anything, it's for extra protection)
+app.secret_key = 'anystring'
 
+# Enter your database connection details below
+app.config['MYSQL_HOST'] = '2young.mysql.pythonanywhere-services.com'
+app.config['MYSQL_USER'] = '2young'
+app.config['MYSQL_PASSWORD'] = 'Young@32494971'
+app.config['MYSQL_DB'] = '2young$COMP639'
+app.config['MYSQL_PORT'] = 3306
 
-from . import util,forms,views
+# app.config['MYSQL_HOST'] = 'localhost'
+# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = ''
+# app.config['MYSQL_DB'] = 'rental'
+# app.config['MYSQL_PORT'] = 3306
+
+from . import views
 
 
